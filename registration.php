bf22bfe4921f2064d99384d5c5ccbca72a1ffd49
NastiaR <hotredmulled@gmail.com>
2014-08-25 15:18:38 +0300

    php

--- registration.php
+++ registration.php
@@ -67,49 +67,52 @@
 		</style>
 	</head>
 	<body>
-	<?php 
-include ('db.php');
- $email = "email@phpguru.com.ua";
- $login = "sasha";
- $DOB = "2001-03-06";
- $pass = "12345";
- $sql = 'INSERT INTO users (email, login, DOB, pass)
- VALUES("'.$email.'", "'.$login.'", "'.$DOB.'", "'.$pass.'")';
-?>
-		<h1 class="pc">Регистрация</h1>
-		<p align="center"><a href="index.html">Щёлкните здесь для перехода на vk.loc</a></p>
-			<table class="table1">
-				<form method="POST">
+		<?php 
+			if(!empty($_POST))
+				{
+					$db = new PDO('mysql:host=localhost;dbname=vk;charset=utf8', 'root', '');
+					$a=$db->query('insert into users (email, Login, DOB, Pass) values ("' . $_POST['email'] . '", "' . $_POST['login'] . '", "' . $_POST['DOB'] . '","' . $_POST['pass'] . '")');
+					$thnks = $_POST['login'] . '! Thank you for registration on our site!';
+				}
+			if(isset($thnks)) 
+				{
+					echo '<h2>' . $thnks . '</h2>';
+				}
+		?>
+			<h1 class="pc">Регистрация</h1>
+			<p align="center"><a href="index.php">Щёлкните здесь для перехода на vk.loc</a></p>
+				<table class="table1">
+					<form method="POST">
+						<tr>
+							<td>Введите email:</td>
+							<td><input type = "text" placeholder = "Поле для ввода email"  size = "40" name="email" required></td>
+						</tr>
+						<tr>
+							<td>Введите логин:</td>
+							<td><input type = "text" maxlength = "12" placeholder = "Поле для ввода логина" size = "40" name="login" required></td>
+						</tr>
+						<tr>
+							<td>Введите дату рождения:</td>
+							<td><input type = "text" placeholder = "Формат ввода даты: YYYY-DD-MM"  size = "40" name="DOB" required></td>
+						</tr>
+						<tr>
+							<td>Введите пароль:</td>
+							<td><input type = "password" maxlength = "12" placeholder = "Поле для ввода пароля" size = "40" name="pass" required></td>
+						<tr>
+							<td colspan="2"><input class="button " type="submit" value="Отправить"></td>
+						</tr>
+						</tr>
+					</form>
+				</table>
+				<table class="pic1">
 					<tr>
-						<td>Введите email:</td>
-						<td><input type = "text" placeholder = "Поле для ввода email"  size = "40" name="email" required></td>
+						<td><img class="big" src="images/jasmin.jpg" alt="Pict"></td>
+						<td><img class="big" src="images/sm.jpg" alt="Pict1"></td>
+						<td><img class="big" src="images/all.jpg" alt="Pict2"></td>
+						<td><img class="big" src="images/mar.jpg" alt="Pict3"></td>
+						<td><img class="big" src="images/mfun.jpg" alt="Pict4"></td>
+						<td><img class="big" src="images/lilm.jpg" alt="Pict5"></td>
 					</tr>
-					<tr>
-						<td>Введите логин:</td>
-						<td><input type = "text" maxlength = "12" placeholder = "Поле для ввода логина" size = "40" name="login" required></td>
-					</tr>
-					<tr>
-						<td>Введите дату рождения:</td>
-						<td><input type = "text" placeholder = "Поле для ввода даты рождения"  size = "40" name="email" required></td>
-					</tr>
-					<tr>
-						<td>Введите пароль:</td>
-						<td><input type = "password" maxlength = "12" placeholder = "Поле для ввода пароля" size = "40" name="Pass" required></td>
-					<tr>
-						<td colspan="2"><input class="button " type="submit" value="Отправить"></td>
-					</tr>
-					</tr>
-				</form>
-			</table>
-			<table class="pic1">
-				<tr>
-					<td><img class="big" src="images/jasmin.jpg" alt="Pict"></td>
-					<td><img class="big" src="images/sm.jpg" alt="Pict1"></td>
-					<td><img class="big" src="images/all.jpg" alt="Pict2"></td>
-					<td><img class="big" src="images/mar.jpg" alt="Pict3"></td>
-					<td><img class="big" src="images/mfun.jpg" alt="Pict4"></td>
-					<td><img class="big" src="images/lilm.jpg" alt="Pict5"></td>
-				</tr>
-			</table>
+				</table>
 	</body>
 </html>

ecbbf75e4f216a8cfe5e43bab5a52cac8e277b80
NastiaR <hotredmulled@gmail.com>
2014-09-03 18:30:15 +0300

    some changes of structure

--- template/registration.php
+++ template/registration.php
@@ -3,47 +3,39 @@
 	<head>
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="E:\xampp\htdocs\vk\CSS\styles.css">
+		<?php include('E:/xampp/htdocs/vk/template/header.php');?>
 	</head>
 	<body>
 		<?php 
-			include 'E:\xampp\htdocs\vk\classes\User.php';
 			if(!empty($_POST))
+			{
+				$user= new User;
+				$check = $user->registration($_POST);
+				$thnks=$user->result;
+				if($check)
 				{
-					$user= new User;
-					$user->registration($_POST);
-					$thnks=$user->result;
+					header("Location: index.php");
+					die;
 				}
-				
-			if(isset($thnks)) 
-				{
-					echo '<h2>' . $thnks . '</h2>';
-				}
+				echo '<h2>' . $thnks . '</h2>';
+			}				
 		?>
-			<form role="form" method="post">
-			<h1 class="pc">Регистрация</h1>
-				<div class="table1">
-					<input type = "text" placeholder = "Поле для ввода email"  size = "60" name="email" required>
-				</div>
-				<div class="table1">
-					<input type = "text" maxlength = "12" placeholder = "Поле для ввода логина" size = "60" name="login" required>
-				</div>
-				<div class="table1">
-					<input type = "text" placeholder = "Формат ввода даты: YYYY-DD-MM"  size = "60" name="DOB" required>
-				</div>
-				<div class="table1">
-					<input type = "password" maxlength = "12" placeholder = "Поле для ввода пароля" size = "60" name="pass" required>
-				</div>
-					<input class="button " type="submit" value="Отправить">
-					</form>
-				<table class="pic1">
-					<tr>
-						<td><img class="big" src="template/images/jasmin.jpg" alt="Pict"></td>
-						<td><img class="big" src="template/images/sm.jpg" alt="Pict1"></td>
-						<td><img class="big" src="template/images/all.jpg" alt="Pict2"></td>
-						<td><img class="big" src="template/images/mar.jpg" alt="Pict3"></td>
-						<td><img class="big" src="template/images/mfun.jpg" alt="Pict4"></td>
-						<td><img class="big" src="template/images/lilm.jpg" alt="Pict5"></td>
-					</tr>
-				</table>
+			<form role="form"  method="post">
+				<h1 class="pc">Регистрация</h1>
+					<div class="table1">
+						<input type = "text" placeholder = "Поле для ввода email"  size = "60" name="email" required>
+					</div>
+					<div class="table1">
+						<input type = "text" maxlength = "12" placeholder = "Поле для ввода логина" size = "60" name="login" required>
+					</div>
+					<div class="table1">
+						<input type = "text" placeholder = "Формат ввода даты: YYYY-DD-MM"  size = "60" name="DOB" required>
+					</div>
+					<div class="table1">
+						<input type = "password" maxlength = "12" placeholder = "Поле для ввода пароля" size = "60" name="pass" required>
+					</div>
+						<input class="button " type="submit" value="Отправить">
+			</form>				
+<?php include('E:/xampp/htdocs/vk/template/footer.php'); ?>
 	</body>
 </html>

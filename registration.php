7e85f93897f4990892ed46803feb938e6b1820ef
NastiaR <hotredmulled@gmail.com>
2014-09-03 18:31:30 +0300

    registration file

--- registration.php
+++ registration.php
@@ -3,9 +3,6 @@ session_start();
 include 'E:\xampp\htdocs\vk\classes\User.php';
 if( isset( $_SESSION['user_id'] ) )
 {
-	// тут ничего не будет пока мы туда не положем юзер айди
-	// ёзер залогинен подключим файл приветсви€ залогиненного юзвер€
-	// include();
 	echo 'Welcome page should be here';
 }
 else

d11815f6ab5975ec519c74cfc2e93d0414b500e2
NastiaR <hotredmulled@gmail.com>
2014-09-04 16:33:27 +0300

    Logout

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php
 session_start();
 include 'E:\xampp\htdocs\vk\classes\User.php';
+
+//проверка введен ли эмейл
+
 if (isset($_POST['email']))
 {
 	$USER = new User;
 	$auth_user_id = $USER->get_user_id($_POST);
+	
+	//проверка соответствия введенных данных данным в БД
 	
 	if($auth_user_id)
 	{
@@ -15,14 +20,25 @@ if (isset($_POST['email']))
 		echo "Wrong email or password.";
 	}
 } 
+
+//проверка для логаута
+
+if(isset ($_GET['logout']))
+	{
+		unset($_SESSION['user_id']);
+	}
 ?>
 <html>
 	<body>
+	<link rel="stylesheet" href="E:\xampp\htdocs\vk\CSS\index.css">
 	<?php
+	
+	//проверка на существование сессии
+	
 	if(isset($_SESSION['user_id']) )
 	{
-		echo 'Welcome page should be here';
-		die;
+		echo 'You authorized, <a href="index.php?logout">Log out</a>';
+		echo '</br>';
 	}
 	else
 	{
